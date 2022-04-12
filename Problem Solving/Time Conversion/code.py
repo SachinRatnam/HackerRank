f649129fb5efeb61b91aba37839634560feb38e0
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-04-12 14:47:37 +0530

    Update code.py

--- Problem Solving/Time Conversion/code.py
+++ Problem Solving/Time Conversion/code.py
@@ -5,4 +5,7 @@ def timeConversion(s):
     elif s == '12:00:00PM':
         return '12:00:00'
     elif 'AM' in s:
-        return 
+        return s[:8]
+    elif 'PM' in s:
+        x = int(s[:2])+ 12 
+        return str(x) + s[2:8]
