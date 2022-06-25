4048a35a26f7883eb70a46667bcab7867aa1dda2
SachinRatnam <77437691+SachinRatnam@users.noreply.github.com>
2022-06-25 14:23:44 +0530

    Create code.py

---
+++ Problem Solving/Compare the Triplets/code.py
@@ -0,0 +1,10 @@
+def compareTriplets(a, b):
+    # Write your code here
+    a_point = 0
+    b_point = 0
+    for i in range(len(a)):
+        if (a[i] > b[i]):
+            a_point += 1
+        elif  (a[i] < b[i]):
+            b_point += 1
+    return [a_point,b_point]
